66fe84d0d65c67204999b86859fce70136e95f0f
Yarish Bohdan <yhbohdan@gmail.com>
2023-01-12 12:22:00 +0200

    Criate object personalMovieDB

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let firstNumber = 10
-let secondNumber = 10
+const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '')
 
-console.log(firstNumber++);
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', '')
+const b = prompt('На сколько оцените его?', '') 
+const c = prompt('Один из последних просмотренных фильмов?', '')
+const d = prompt('На сколько оцените его?', '') 
+
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
+
+console.log(personalMovieDB);
+
+
+
